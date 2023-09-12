39e8f286cf22d86935b51ec4d5bfa0e103be8016
Hazik khan <108752662+galalio445@users.noreply.github.com>
2023-09-12 21:51:38 +0500

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,34 +1,18 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-let handler = async (m, { text, usedPrefix, command }) => {
-  
-  if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response.`;
-  }
-
-  if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text;
-  }
-
-  try {
-    conn.sendPresenceUpdate('composing', m.chat);
-    const prompt = encodeURIComponent(text);
-     const endpoint = `https://api.lolhuman.xyz/api/openai-turbo?apikey${lolkeysapi}&text=${prompt}&system=${prompt}`;
-
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    const abc = data.result; 
-
-   m.reply(abc);
-
-  } catch (error) {
-    console.error('Error: use chat chatgpt or openai command', error);
-    throw `*ERROR*`;
-  }
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `Enter text or your question?`
+    let res = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${prompt}&system=hey`)
+    let zel = await res.json()
+   m.reply(zel.result)  catch (e) {
+         m.reply("Sorry we have faced server side error use Openai or ai command")
+      }
 };
-handler.help = ["ai"]
-handler.command = ['ai'];
-handler.diamond = false;
+
+handler.help = ['chatgpt']
+handler.tags = ['ai']
+handler.command = /^(ai|openai)$/i
 
 export default handler;
 
+ 
